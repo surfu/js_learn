d09239b5b970a4abe5872beca6850eb0a515f3f6
surfu <krevsunaliona@gmail.com>
2026-09-22 02:29:22 +0300

    to-do

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-let output = document.querySelector("#out");
-let input1 = parseInt(document.querySelector("#num1").value);
-let input2 = parseInt(document.querySelector("#num2").value);
-let sign = document.querySelector("#sign");
-let butt = document.querySelector("#calcBtn");
-let sum;
+let butt = document.querySelector("#but");
+let input = document.querySelector("#inp");
+let box = document.querySelector(".container");
 
 butt.addEventListener("click", () => {
-    let opt = sign.value
-    if (opt === "+") {
-        sum = input1 + input2;
-    } else if (opt === "-") {
-        sum = input1 - input2;
-    } else if (opt === "*") {
-        sum = input1 * input2;
-    } else if (opt === "/") {
-        sum = input1 / input2;
-    }
-    output.textContent = sum;
-})
+    let childBox = document.createElement("div");
+
+    let mark = document.createElement("input");
+    mark.type = "checkbox";
+
+    let newTask = document.createElement("p");
+    newTask.textContent = input.value;
+    newTask.style.background = "lightyellow";
+
+    mark.addEventListener("change", () => {
+        if (mark.checked){
+            newTask.style.color = "red";
+            newTask.style.textDecoration = "line-through";
+        }else{
+            newTask.style.color = "black";
+        }
+    });
+    childBox.appendChild(mark);
+    childBox.appendChild(newTask);
+    box.appendChild(childBox);
+    input.value = "";
+});
